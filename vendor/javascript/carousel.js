826e77716f229372e39b7394d6888bd7336c6b44
Annabelle <annabellellis1999@gmail.com>
2021-05-20 22:26:42 -0500

    Carousel Finished!

--- vendor/javascript/carousel.js
+++ vendor/javascript/carousel.js
@@ -30,6 +30,22 @@ const updateDots = (currentDot,targetDot)=>{
     currentDot.classList.remove('current-slide');
     targetDot.classList.add('current-slide');
 };
+
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex)=>{
+    if(targetIndex === 0){
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+
+    }else if(targetIndex === slides.length -1){
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    }else{
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+
+    }
+}
 // when I click, move slides to the left 
 prevButton.addEventListener('click', e =>{
     // find current slide
@@ -41,9 +57,13 @@ prevButton.addEventListener('click', e =>{
 
     const currentDot = dotsNav.querySelector('.current-slide');
     const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
 
     moveToSlide(track,currentSlide,prevSlide);
     updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+
 
 });
 
@@ -57,10 +77,13 @@ nextButton.addEventListener('click', e =>{
     const nextSlide = currentSlide.nextElementSibling;
     const currentDot = dotsNav.querySelector('.current-slide');
     const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
 
     console.log(nextSlide);
     moveToSlide(track,currentSlide,nextSlide);
     updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+
 });
 
 // nextButton.addEventListener('click', e =>{
@@ -107,5 +130,8 @@ dotsNav.addEventListener('click', e => {
     moveToSlide(track, currentSlide, targetSlide);
 
     updateDots(currentSlide, targetDot);
+
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+    
 
 });
